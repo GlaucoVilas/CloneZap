7c719846104dfe99809367af86c8e6b7858f843b
Glauco <glaucovinicios@gmail.com>
2017-10-17 18:55:41 -0200

    SlidingTabLayout and SlidingTabStrip

--- app/src/main/java/com/firebase/cursoandroid/clonezap/activity/MainActivity.java
+++ app/src/main/java/com/firebase/cursoandroid/clonezap/activity/MainActivity.java
@@ -2,14 +2,18 @@ package com.firebase.cursoandroid.clonezap.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 
+import com.firebase.cursoandroid.clonezap.Adapter.TabAdapter;
 import com.firebase.cursoandroid.clonezap.R;
 import com.firebase.cursoandroid.clonezap.config.ConfiguracaoFirebase;
+import com.firebase.cursoandroid.clonezap.helper.SlidingTabLayout;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,18 +21,31 @@ public class MainActivity extends AppCompatActivity {
     private Toolbar toolbar;
     private FirebaseAuth auth;
 
+    private SlidingTabLayout slidingTabLayout;
+    private ViewPager viewPager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         auth = ConfiguracaoFirebase.getFirebaseAuth();
-
-        toolbar = (Toolbar) findViewById(R.id.toolbar_principal
-        );
+        toolbar = (Toolbar) findViewById(R.id.toolbar_principal);
         toolbar.setTitle("CloneZap");
         setSupportActionBar(toolbar);
 
+        slidingTabLayout = (SlidingTabLayout) findViewById(R.id.stl_tabs);
+        viewPager = (ViewPager) findViewById(R.id.vp_pagina);
+
+        //Configurar Sliding tabs
+        slidingTabLayout.setDistributeEvenly(true);
+        slidingTabLayout.setSelectedIndicatorColors(ContextCompat.getColor(this, R.color.colorAccent));
+
+        //Configurar Adapter
+        TabAdapter tabAdapter = new TabAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(tabAdapter);
+
+        slidingTabLayout.setViewPager(viewPager);
     }
 
     @Override
